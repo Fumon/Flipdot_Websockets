afdee51833388361077d401bdb7957a5b30e10d3
Jade Bilkey <herself@thefumon.com>
2014-12-27 18:21:27 -0500

    Changed to allow multiple boards on arduino

--- squarish_flip_serial/squarish_flip_serial.ino
+++ squarish_flip_serial/squarish_flip_serial.ino
@@ -1,14 +1,14 @@
 #define dmask 0x7C
 #define bmask 0x1F
-#define cmask 0x2F
+#define cmask 0x3F
 
 #define dirpin 13
 
 #define state1 0x5
 #define state2 0x6
 
-#define dataDelayMicro 20
-#define etime 500
+#define dataDelayMicro 10
+#define etime 200
 
 #define first_flip_delay 300
 
@@ -63,7 +63,7 @@ void _d(int yellow) {
 
 void _f(int yellow, int panel) {
   delayMicroseconds(dataDelayMicro);
-  PORTC |= (0x4 << (panel > 0 ? 1 : 0)) | (0x1 << (~yellow & 0x1));
+  PORTC |= (0x4 << panel) | (0x1 << (~yellow & 0x1));
   delayMicroseconds(etime);
   PORTC &= ~cmask;
 }
@@ -77,7 +77,7 @@ void flip(int x, int y, int yellow, int panel) {
 
 void clear_dots(int yellow) {
   _d(yellow);
-  for(int panel = 0; panel < 2; panel++) {
+  for(int panel = 0; panel < 3; panel++) {
     for(int y = 0; y < 24; y++) {
       _y(y);
       for(int x = 0; x < 28; x++) {
@@ -91,20 +91,24 @@ void clear_dots(int yellow) {
 
 void serialEvent() {
   if(Serial.available() >= 3) {
-    int buffer[3] = {};
+    int buffer[3] = {
+    };
     buffer[0] = (int)Serial.read();
     buffer[1] = (int)Serial.read();
     buffer[2] = (int)Serial.read();
     if(buffer[2] == 0xF0) {
       clear_dots(1);
-    } else if(buffer[2] == 0xE0) {
+    } 
+    else if(buffer[2] == 0xE0) {
       clear_dots(0);
-    } else if(buffer[2] == 0xD0){ // Ack request
-      delay(1);
+    } 
+    else if(buffer[2] == 0xD0){ // Ack request
+      //delay(1);
       Serial.print("H");
     }
     else {
-      flip(buffer[0], buffer[1], buffer[2] & 0x1, buffer[2] >> 1);
+      flip(buffer[0], buffer[1], buffer[2] & 0x1, buffer[2] >> 1
+      );
     }
   }
 }
@@ -125,4 +129,5 @@ void setup() {
 void loop() {
   ;
 }
+
 
